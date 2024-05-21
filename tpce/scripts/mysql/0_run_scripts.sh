2e05ae78c5f3f46c530622760476cf718764e1f2
dlddu <39251873+dlddu@users.noreply.github.com>
2024-05-21 16:22:54 +0000

    chore: Update MySQL user in script for running SQL files

--- tpce/scripts/mysql/0_run_scripts.sh
+++ tpce/scripts/mysql/0_run_scripts.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mysql -u tpce -ptpce -D tpce -h db < 1_create_table.sql &&
-mysql -u tpce -ptpce -D tpce -h db < 1.5_enable_load_local_data.sql &&
-mysql -u tpce -ptpce -D tpce -h db --local-infile=1 < 2_load_data.sql &&
-mysql -u tpce -ptpce -D tpce -h db < 3_create_index.sql &&
-mysql -u tpce -ptpce -D tpce -h db < 4_create_fk.sql &&
-mysql -u tpce -ptpce -D tpce -h db < 5_create_sequence.sql
+mysql -u root -ptpce -D tpce -h db < 1_create_table.sql &&
+mysql -u root -ptpce -D tpce -h db < 1.5_enable_load_local_data.sql &&
+mysql -u root -ptpce -D tpce -h db --local-infile=1 < 2_load_data.sql &&
+mysql -u root -ptpce -D tpce -h db < 3_create_index.sql &&
+mysql -u root -ptpce -D tpce -h db < 4_create_fk.sql &&
+mysql -u root -ptpce -D tpce -h db < 5_create_sequence.sql
