844f66e0f9017db1af0775fdca53553ca8c4e1e8
dlddu <39251873+dlddu@users.noreply.github.com>
2024-05-21 22:22:35 +0900

    chore: Update build_tpce.sh to use absolute path for make command

--- build_tpce.sh
+++ build_tpce.sh
@@ -2,4 +2,4 @@
 
 docker build . -t big-data
 mkdir ./tpce/bin ./tpce/flat_out ./tpce/obj ./tpce/lib
-docker run -it --rm -v $(pwd)/tpce:/tpce big-data bash -c 'cd /tpce/prj && make'
+docker run -it --rm -v $(pwd)/tpce:/tpce -w /tpce/prj big-data make
