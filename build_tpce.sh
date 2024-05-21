c09d01b28d1d1f35e4c58412c1221bb8f6e2c2f4
dlddu <39251873+dlddu@users.noreply.github.com>
2024-05-22 00:08:03 +0900

    chore: Update database configuration and initialization

--- build_tpce.sh
+++ build_tpce.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-docker build . -t big-data
-mkdir ./tpce/bin ./tpce/flat_out ./tpce/obj ./tpce/lib
-docker run -it --rm -v $(pwd)/tpce:/tpce -w /tpce/prj big-data make
+docker build . -t big-data &&
+mkdir ./tpce/bin ./tpce/flat_out ./tpce/obj ./tpce/lib;
+docker run -it --rm -v $(pwd)/tpce:/tpce -w /tpce/prj big-data make &&
+docker run -it --rm -v $(pwd)/tpce:/tpce -w /tpce big-data ./bin/EGenLoader -c 1000 -t 1000 -f 288000 -w 1 &&
+docker compose down -v && docker compose up -d &&
+docker build . -f Dockerfile_SimpleTest -t percona
+docker run -it --rm -w /tpce-mysql percona
